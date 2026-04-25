175347d059f5fc12c60d507c261aafb6bd78359a
d1aniel <dcamilorodriguez@uniguajira.edu.co>
2026-04-25 09:54:32 -0500

    lo que sea

--- agricultura_inteligente/settings.py
+++ agricultura_inteligente/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
