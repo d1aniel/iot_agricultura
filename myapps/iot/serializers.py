e99de80970f916c4915b685af5af96adb7c2a137
d1aniel <dcamilorodriguez@uniguajira.edu.co>
2026-05-24 15:07:02 -0500

    mqtt co

--- myapps/iot/serializers.py
+++ myapps/iot/serializers.py
@@ -10,9 +10,30 @@ class NodoIoTSerializer(serializers.ModelSerializer):
 
 
 class SensorSerializer(serializers.ModelSerializer):
+    nodo_nombre = serializers.CharField(source='nodo.nombre', read_only=True)
+    codigo_nodo = serializers.CharField(source='nodo.codigo_nodo', read_only=True)
+    parcela_nombre = serializers.CharField(source='nodo.parcela.nombre', read_only=True)
+    finca_nombre = serializers.CharField(source='nodo.parcela.finca.nombre', read_only=True)
+
     class Meta:
         model = Sensor
-        fields = '__all__'
+        fields = (
+            'id',
+            'nodo',
+            'nodo_nombre',
+            'codigo_nodo',
+            'parcela_nombre',
+            'finca_nombre',
+            'nombre',
+            'tipo_sensor',
+            'modelo',
+            'unidad_medida',
+            'pin_conexion',
+            'valor_minimo',
+            'valor_maximo',
+            'estado',
+            'fecha_instalacion',
+        )
 
 
 class LecturaSensorSerializer(serializers.ModelSerializer):
