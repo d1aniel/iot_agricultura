79e375f7401848153a029ff06c33e85cb19cb68e
d1aniel <dcamilorodriguez@uniguajira.edu.co>
2026-05-24 17:16:42 -0500

    sera que si?

--- myapps/iot/serializers.py
+++ myapps/iot/serializers.py
@@ -12,6 +12,8 @@ class NodoIoTSerializer(serializers.ModelSerializer):
 class SensorSerializer(serializers.ModelSerializer):
     nodo_nombre = serializers.CharField(source='nodo.nombre', read_only=True)
     codigo_nodo = serializers.CharField(source='nodo.codigo_nodo', read_only=True)
+    parcela_id = serializers.IntegerField(source='nodo.parcela_id', read_only=True)
+    finca_id = serializers.IntegerField(source='nodo.parcela.finca_id', read_only=True)
     parcela_nombre = serializers.CharField(source='nodo.parcela.nombre', read_only=True)
     finca_nombre = serializers.CharField(source='nodo.parcela.finca.nombre', read_only=True)
 
@@ -22,6 +24,8 @@ class SensorSerializer(serializers.ModelSerializer):
             'nodo',
             'nodo_nombre',
             'codigo_nodo',
+            'parcela_id',
+            'finca_id',
             'parcela_nombre',
             'finca_nombre',
             'nombre',
